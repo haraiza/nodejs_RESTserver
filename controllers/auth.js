5b8aff4da1eedf450629f19bbce80cabfdad8e35
humberto-desk <humberto_araiza@outlook.com>
2022-01-11 00:01:13 -0800

    cap 141 - Login de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,52 @@
 const { response } = require('express');
+const Usuario = require('../models/usuario');
+const bcryptjs = require('bcryptjs');
+
+const login = async (req, res = response) => {
+
+    const { correo, password } = req.body;
+
+    try {
+
+        // Verificar si el email existe con
+        const usuario = await Usuario.findOne({ correo });
+        if (!usuario) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - correo'
+            })
+        }
 
 
-const login = (req, res = response) => {
-    res.json({
-        msg: 'Login ok'
-    })
+        // Verificar si el usuario esta activo
+        if (!usuario.estado) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - estado: false'
+            })
+        }
+
+
+        // Verificar la contraseña
+        const validPassword = bcryptjs.compareSync(password, usuario.password);
+        if(!validPassword) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - password'
+            })
+        }
+
+        // Generar el JWT
+
+
+        res.json({
+            msg: 'Login ok'
+        })
+    } catch (error) {
+
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Hable conel administrador'
+        });
+    }
+
 }
 
 module.exports = {
