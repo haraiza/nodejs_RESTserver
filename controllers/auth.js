c0ff2731d86d386f3be1a436222f5d4322355f78
humberto-desk <humberto_araiza@outlook.com>
2022-01-13 22:21:34 -0800

    cap 156 - Rutas para manejar autenticación Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,56 +1,60 @@
-const { response } = require('express');
-const Usuario = require('../models/usuario');
-const bcryptjs = require('bcryptjs');
-const { generarJWT } = require('../helpers/generarJWT');
+const { response } = require("express");
+const Usuario = require("../models/usuario");
+const bcryptjs = require("bcryptjs");
+const { generarJWT } = require("../helpers/generarJWT");
 
 const login = async (req, res = response) => {
+  const { correo, password } = req.body;
 
-    const { correo, password } = req.body;
-
-    try {
-
-        // Verificar si el email existe con
-        const usuario = await Usuario.findOne({ correo });
-        if (!usuario) {
-            return res.status(400).json({
-                msg: 'Usuario / Password no son correctos - correo'
-            })
-        }
-
-
-        // Verificar si el usuario esta activo
-        if (!usuario.estado) {
-            return res.status(400).json({
-                msg: 'Usuario / Password no son correctos - estado: false'
-            })
-        }
-
-
-        // Verificar la contraseña
-        const validPassword = bcryptjs.compareSync(password, usuario.password);
-        if (!validPassword) {
-            return res.status(400).json({
-                msg: 'Usuario / Password no son correctos - password'
-            })
-        }
-
-        // Generar el JWT
-        const token = await generarJWT(usuario.id);
-
-        res.json({
-            token,
-            usuario
-        })
-    } catch (error) {
-
-        console.log(error);
-        return res.status(500).json({
-            msg: 'Hable conel administrador'
-        });
+  try {
+    // Verificar si el email existe con
+    const usuario = await Usuario.findOne({ correo });
+    if (!usuario) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - correo",
+      });
     }
 
-}
+    // Verificar si el usuario esta activo
+    if (!usuario.estado) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - estado: false",
+      });
+    }
+
+    // Verificar la contraseña
+    const validPassword = bcryptjs.compareSync(password, usuario.password);
+    if (!validPassword) {
+      return res.status(400).json({
+        msg: "Usuario / Password no son correctos - password",
+      });
+    }
+
+    // Generar el JWT
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      token,
+      usuario,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      msg: "Hable conel administrador",
+    });
+  }
+};
+
+const googleSignIn = async (req, res = response, next) => {
+  const { id_token } = req.body;
+
+  res.json({
+    msg: "todo bien",
+    id_token
+  });
+};
 
 module.exports = {
-    login
-}
+  login,
+  googleSignIn,
+};
