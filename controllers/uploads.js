d807d299ab4fc616f9655f33373f15e70c9f33f9
humberto-desk <humberto_araiza@outlook.com>
2022-01-23 10:45:32 -0800

    187. Crear carpetas destino

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -5,13 +5,18 @@ const cargarArchivo = async (req, res = response) => {
   if (!req.files || Object.keys(req.files).length === 0 || !req.files.archivo) {
     return res.status(400).json({ msg: "No hay archivos que subir" });
   }
+
   try {
-    const pathCompleto = await subirArchivo(req.files);
+    // Para subit textos a la carpeta 'textos'
+    //const pathCompleto = await subirArchivo(req.files, ["txt", "md"], "textos");
+
+    // Para subit imagenes a la carpeta 'imgs'
+    const pathCompleto = await subirArchivo(req.files, undefined, "imgs");
     res.json({
       nombre: pathCompleto,
     });
   } catch (err) {
-    return res.json({
+    return res.status(400).json({
       msg: err,
     });
   }
